d8a29b4c92c73ec51393073b942886a0a318a2da
AxelVZM <axelantoniomarco2005@gmail.com>
2026-01-04 00:55:28 -0500

    Desplegar App

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+      origin: [
+        process.env.FRONTEND_URL || 'http://localhost:3001',
+        'https://*.railway.app'
+      ],
+      credentials: true,
+    });
   // Habilitar validación global con transformación para que los DTOs
   // conviertan y validen tipos (por ejemplo latitud/longitud strings -> number)
   app.useGlobalPipes(
