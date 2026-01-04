096af8153f1ca7c0cac3dd30f8dae4a64fa2198b
AxelVZM <axelantoniomarco2005@gmail.com>
2026-01-04 12:18:47 -0500

    Usar variable de entorno para API_URL

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
 // Obtener una denuncia por su ID
 export async function getDenunciaPorId(id: string | number) {
